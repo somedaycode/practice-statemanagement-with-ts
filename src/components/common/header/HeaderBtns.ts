d38026892de94486045ce9c11c2e368e7e6bb3f6
somedaycode <somedaycode@gmail.com>
2021-08-25 03:12:19 +0900

    feat: 헤더 버튼 추가

---
+++ src/components/common/header/HeaderBtns.ts
@@ -0,0 +1,10 @@
+import Component from '@src/core/Component';
+
+export default class HeaderBtns extends Component {
+  template() {
+    return `
+    <button>back</button>
+    <button>new</button>
+    `;
+  }
+}
