5ff0e52d20e2f47a4ab95544c597eb013e12ab80
somedaycode <somedaycode@gmail.com>
2021-08-25 03:49:11 +0900

    feat: 전역상태 옵저버 속성 값 이름 변경

--- src/lib/observer.ts
+++ src/lib/observer.ts
@@ -1,6 +1,7 @@
 type GlobalStateType = {
-  [key: string]: { _state: unknown; _observers: Map<string, Function> };
+  [key: string]: { _state: any; _observers: Map<string, Function> };
 };
+
 const globalState: GlobalStateType = {};
 
 const subscribe = (key: string, observer: Function) =>
@@ -9,36 +10,29 @@ const subscribe = (key: string, observer: Function) =>
 const _notify = (key: string) =>
   globalState[key]._observers.forEach((observer: Function) => observer());
 
-const initState = ({
-  key,
-  defaultValue
-}: {
-  key: string;
-  defaultValue: unknown;
-}) => {
+const initState = <T>({ key, value }: { key: string; value: T }): string => {
   if (key in globalState) throw Error('이미 존재하는 key값 입니다.');
   globalState[key] = {
-    _state: defaultValue,
+    _state: value,
     _observers: new Map()
   };
   return key;
 };
 
-const getState = (key: string) => {
+const getState = <T>(key: string): T => {
   if (!(key in globalState)) throw Error('존재하지 않는 key값 입니다.');
   return globalState[key]._state;
 };
 
-const setState = (key: string) => (newState: unknown) => {
+const setState = <T>(key: string, newState: ((arg: T) => T) | T): void => {
   if (!(key in globalState)) throw Error('존재하지 않는 key값 입니다.');
 
-  if (typeof newState === 'function') {
-    const state = getState(key);
+  if (newState instanceof Function) {
+    const state = getState<T>(key);
     globalState[key]._state = newState(state);
   } else {
     globalState[key]._state = newState;
   }
-
   _notify(key);
 };
 
