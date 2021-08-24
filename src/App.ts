a4a4e098dd563b86214c300183e59c753f257ef0
somedaycode <somedaycode@gmail.com>
2021-08-24 23:27:11 +0900

    feat: 라우터  경로 설정

--- src/App.ts
+++ src/App.ts
@@ -1,12 +1,16 @@
 import Router from '@src/lib/Router';
 import Component from '@src/core/Component';
 
-import Home from './components/Home';
-import SecondPage from './components/SecondPage';
+import Home from '@src/pages/Home';
+import Alarm from '@src/pages/Alarm';
+import Memo from '@src/pages/Memo';
+import Picture from '@src/pages/Picture';
 
 const routerPath = {
   '/': Home,
-  '/second': SecondPage
+  '/alarm': Alarm,
+  '/memo': Memo,
+  '/picture': Picture
 };
 export const router = new Router(routerPath);
 export default class App extends Component {
