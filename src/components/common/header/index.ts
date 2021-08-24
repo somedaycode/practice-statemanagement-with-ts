91912ac7d38c983c704bf6b03dc9a3addb0b4dfd
somedaycode <somedaycode@gmail.com>
2021-08-25 03:10:54 +0900

    feat: Header 컴포넌트에서 date와 btns 컴포넌트 추가 렌더링

--- src/components/common/header/index.ts
+++ src/components/common/header/index.ts
@@ -1,11 +1,20 @@
 import Component from '@src/core/Component';
+import HeaderBtns from './HeaderBtns';
+import HeaderDate from './HeaderDate';
 
 export default class Header extends Component {
   template() {
     return `
-    <button>back</button>
-    <nav class="nav-date">날짜가 들어간다</nav>
-    <button>new</button>
+    <div data-component="headerBtns" class="btns-container"></div>
+    <nav data-component="headerDate" class="nav-date"></nav>
     `;
+  }
+
+  mountChildComponent() {
+    const $btns = this.$target.querySelector('[data-component=headerBtns]');
+    new HeaderBtns($btns as HTMLDivElement);
+
+    const $nav = this.$target.querySelector('[data-component=headerDate]');
+    new HeaderDate($nav as HTMLElement);
   }
 }
