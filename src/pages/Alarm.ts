b3a146deb8de7bfceb5bd1ff3d788b66f4bcad15
somedaycode <somedaycode@gmail.com>
2021-08-26 22:51:11 +0900

    feat: 알람을 위한 페이지 추가

--- src/pages/Alarm.ts
+++ src/pages/Alarm.ts
@@ -1,7 +1,42 @@
 import Component from '@src/core/Component';
+import { setState } from '@src/lib/observer';
+import { headerLocation } from '@src/store/headerStore';
+
+import Header from '@src/components/common/header';
+import { AlarmLists, SettingAlarm } from '@src/components/alarm';
 
 export default class Alarm extends Component {
+  constructor($target: HTMLElement) {
+    super($target);
+    this.keys = [headerLocation];
+    this.subscribe();
+  }
+
   template() {
-    return '<div>알람알람</div>';
+    return `
+    <header class="header" data-component="header"></header>
+    <main class="alarm">
+      <div data-component="settingAlarm" class="alarm__select"></div>
+      <ul data-component="alarmLists" class="alarm__lists"></ul>
+    </main>`;
+  }
+
+  setup() {
+    const setDate = setState(headerLocation);
+    setDate({ isMain: true });
+  }
+
+  mountChildComponent() {
+    const $header = this.$target.querySelector('[data-component=header]');
+    const $settingAlarm = this.$target.querySelector(
+      '[data-component=settingAlarm]'
+    );
+    const $alarmLists = this.$target.querySelector(
+      '[data-component=alarmLists]'
+    );
+
+    new Header($header as HTMLElement);
+    new SettingAlarm($settingAlarm as HTMLDivElement);
+    new AlarmLists($alarmLists as HTMLUListElement);
   }
 }
