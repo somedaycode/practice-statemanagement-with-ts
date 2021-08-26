779a0075d238977bfd4845c62b2a42322df77186
somedaycode <somedaycode@gmail.com>
2021-08-26 22:54:06 +0900

    feat: 페이지 위치에 따른 헤더 버튼 숨김

--- src/components/common/header/HeaderBtns.ts
+++ src/components/common/header/HeaderBtns.ts
@@ -1,10 +1,23 @@
 import Component from '@src/core/Component';
+import { getState } from '@src/lib/observer';
+import { headerLocation } from '@src/store/headerStore';
 
 export default class HeaderBtns extends Component {
+  constructor($target: HTMLDivElement) {
+    super($target);
+    this.keys = [headerLocation];
+    this.subscribe();
+  }
+
   template() {
+    const isMain = this.getHeaderLocation();
     return `
-    <button>back</button>
-    <button>new</button>
+    ${isMain ? '<button>back</button><button>new</button>' : ''}
     `;
+  }
+
+  getHeaderLocation() {
+    const { isMain } = getState(headerLocation);
+    return isMain;
   }
 }
